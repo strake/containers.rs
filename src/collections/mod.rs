788e415c3af170d52bb9aae23481224a1bb69d45
M Farkas-Dyck <strake888@gmail.com>
2017-10-28 23:40:38 -0800

    expose `RawVec`

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -3,11 +3,11 @@
 pub mod b_tree;
 pub mod hash_table;
 pub mod heap;
+pub mod raw_vec;
 pub mod vec;
-
-mod raw_vec;
 
 pub use self::b_tree::BTree;
 pub use self::hash_table::HashTable;
 pub use self::heap::Heap;
+pub use self::raw_vec::RawVec;
 pub use self::vec::Vec;
