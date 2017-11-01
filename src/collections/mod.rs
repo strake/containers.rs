7de8dd3d0eb5055fa48f57f8ced864d0051823be
M Farkas-Dyck <strake888@gmail.com>
2017-10-31 22:42:04 -0800

    hide submodules of `collections`; we already expose the types

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -1,10 +1,10 @@
 //! Collection types
 
-pub mod b_tree;
-pub mod hash_table;
-pub mod heap;
-pub mod raw_vec;
-pub mod vec;
+mod b_tree;
+mod hash_table;
+mod heap;
+mod raw_vec;
+mod vec;
 
 pub use self::b_tree::BTree;
 pub use self::hash_table::HashTable;
