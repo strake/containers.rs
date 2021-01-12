1e2a21cf8cb21bbbb3ad160f9e64d7852bed4232
M Farkas-Dyck <strake888@gmail.com>
2021-01-12 08:34:30 -0800

    stash on tmp-1

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -11,3 +11,22 @@ pub use self::hash_table::HashTable;
 pub use self::heap::Heap;
 pub use self::raw_vec::{RawVec, FixedStorage};
 pub use self::vec::Vec;
+
+#[macro_export]
+macro_rules! vec {
+    // ($x:expr; $n:expr) => ($crate::collections::Vec::replicate($x, $n));
+    () => (Some($crate::collections::Vec::new()));
+    ($x:expr; $n:expr) => ({
+        let mut xs = $crate::collections::Vec::with_capacity($n);
+        if let Some(xs) = &mut xs { for _ in 0..$n { xs.push($x); } }
+        xs
+    });
+    ($($x:expr),+ $(,)?) => ($crate::boxed::Box::new([$($x),+]).and_then($crate::collections::Vec::from).ok());
+}
+
+/// ```
+/// let x = vec![0u8, 1, 5];
+/// let y = vec![0u8; 16];
+/// ```
+#[cfg(doctest)]
+struct VecMacro;
