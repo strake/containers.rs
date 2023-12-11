62f0b4e5c5f4c8ac803401ec585a3848dedce119
M Farkas-Dyck <m@farkas-dyck.ca>
2023-12-11 12:17:42 -0800

    drop stable feature

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
 #![deny(missing_debug_implementations)]
 
 #![feature(coerce_unsized)]
-#![feature(const_fn_trait_bound)]
 #![feature(const_mut_refs)]
 #![feature(dropck_eyepatch)]
 #![feature(unsize)]
